94a6fd6bb9b7d2c716295416ca5369744ea53bad
Generoso Pagano <generoso.pagano@inria.fr>
2014-10-13 16:19:21 +0200

    New update site uploaded

---
+++ src/fr.inria.soctrace.framesoc.updatesite/upload.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+# path of gh-pages checkout
+GHPAGES=../../framesoc.gh-pages/
+
+rm -rf ${GHPAGES}/* 
+cp features plugins web artifacts.jar content.jar index.html site.xml ${GHPAGES}
+
+cd ${GHPAGES}
+
+git add --all
+git commit -m "New update site uploaded"
+git push origin gh-pages
