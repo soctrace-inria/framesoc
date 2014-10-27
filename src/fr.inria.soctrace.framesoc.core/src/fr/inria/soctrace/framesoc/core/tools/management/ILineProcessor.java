a435b363fb6cdae95cf2a4430aceb06fe61d5892
Generoso Pagano <generoso.pagano@inria.fr>
2014-10-27 16:54:32 +0100

    First wrapper implementation.

---
+++ src/fr.inria.soctrace.framesoc.core/src/fr/inria/soctrace/framesoc/core/tools/management/ILineProcessor.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package fr.inria.soctrace.framesoc.core.tools.management;
+
+/**
+ * 
+ * @author "Generoso Pagano <generoso.pagano@inria.fr>"
+ */
+public interface ILineProcessor {
+
+	public void process(String line);
+	
+}
