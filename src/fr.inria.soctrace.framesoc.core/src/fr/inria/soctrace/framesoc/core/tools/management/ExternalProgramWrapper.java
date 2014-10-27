7a9a985c374f268ad17184688fadb620d5deb8c3
Generoso Pagano <generoso.pagano@inria.fr>
2014-10-27 16:54:32 +0100

    First wrapper implementation.

---
+++ src/fr.inria.soctrace.framesoc.core/src/fr/inria/soctrace/framesoc/core/tools/management/ExternalProgramWrapper.java
@@ -0,0 +1,82 @@
+package fr.inria.soctrace.framesoc.core.tools.management;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * TODO
+ * 
+ * @author "Generoso Pagano <generoso.pagano@inria.fr>"
+ */
+public class ExternalProgramWrapper {
+
+	private final static Logger logger = LoggerFactory.getLogger(ExternalProgramWrapper.class);
+
+	/**
+	 * Program command with arguments
+	 */
+	protected List<String> fCommand;
+
+	/**
+	 * Constructor
+	 * 
+	 * @param command
+	 *            program command
+	 * @param arguments
+	 *            command arguments
+	 */
+	public ExternalProgramWrapper(String command, List<String> arguments) {
+		super();
+		if (command == null)
+			throw new NullPointerException();
+		if (arguments == null)
+			throw new NullPointerException();
+		fCommand = new ArrayList<>(arguments.size() + 1);
+		fCommand.add(command);
+		fCommand.addAll(arguments);
+	}
+
+	/**
+	 * Execute the external program, passing the command stdout and stderr to
+	 * the given processor.
+	 * 
+	 * @param monitor
+	 *            progress monitor
+	 * @param processor
+	 *            line processor
+	 * @return the execution status
+	 */
+	public IStatus execute(IProgressMonitor monitor, ILineProcessor processor) {
+		logger.debug("Executing: {}", fCommand);
+
+		try {
+			ProcessBuilder pb = new ProcessBuilder(fCommand);
+			pb.redirectErrorStream(true);
+			pb.redirectOutput(Redirect.PIPE);
+			Process p = pb.start();
+			
+			BufferedReader bri = new BufferedReader(new InputStreamReader(p.getInputStream()));
+			String line = "";
+			while ((line = bri.readLine()) != null) {
+				processor.process(line);
+			}
+			bri.close();
+
+		} catch (IOException e) {
+			System.err.println(e.getMessage());
+			return Status.CANCEL_STATUS;
+		}
+
+		return Status.OK_STATUS;
+	}
+}
---
+++ src/fr.inria.soctrace.tools.importer.otf2/src/fr/inria/soctrace/tools/importer/otf2/reader/Otf2PrintWrapper.java
@@ -0,0 +1,119 @@
+package fr.inria.soctrace.tools.importer.otf2.reader;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Platform;
+import org.osgi.framework.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import fr.inria.soctrace.framesoc.core.tools.management.ExternalProgramWrapper;
+import fr.inria.soctrace.tools.importer.otf2.Activator;
+
+/**
+ * Wrapper for otf2-print program.
+ * 
+ * It looks for the otf2-print executable path in the configuration file
+ * ./<eclipse.dir>/configuration/<plugin.name>/otf2-print.path.
+ * 
+ * If this file is not found, one is created with a default value, pointing to
+ * the precompiled executable (./<plugin.name>/exe/otf2-print).
+ * 
+ * @author "Generoso Pagano <generoso.pagano@inria.fr>"
+ */
+public class Otf2PrintWrapper extends ExternalProgramWrapper {
+
+	private final static Logger logger = LoggerFactory.getLogger(Otf2PrintWrapper.class);
+
+	/**
+	 * Configuration directory
+	 */
+	private final static String CONF_DIR = "configuration" + File.separator + Activator.PLUGIN_ID
+			+ File.separator;
+
+	/**
+	 * Configuration file
+	 */
+	private final static String CONF_FILE = CONF_DIR + "otf2-print.path";
+
+	/**
+	 * Default oft2-print executable location
+	 */
+	private static final String DEFAULT_PATH = "exe" + File.separator + "otf2-print";
+
+	/**
+	 * Constructor
+	 * 
+	 * @param arguments
+	 *            program arguments
+	 */
+	public Otf2PrintWrapper(List<String> arguments) {
+		super(readPath(), arguments);
+	}
+
+	/**
+	 * Read the executable path from the configuration file
+	 * 
+	 * @return the executable path
+	 */
+	private static String readPath() {
+
+		String eclipseDir = Platform.getInstallLocation().getURL().getPath();
+
+		// configuration directory
+		File dir = new File(eclipseDir + CONF_DIR);
+		if (!dir.exists())
+			dir.mkdir();
+
+		// configuration file
+		String absolutePath = eclipseDir + CONF_FILE;
+		File file = new File(absolutePath);
+
+		try {
+			// executable path
+			Bundle bundle = Platform.getBundle(Activator.PLUGIN_ID);
+			Path path = new Path(DEFAULT_PATH);
+			URL fileURL = FileLocator.find(bundle, path, null);
+			String executablePath = FileLocator.resolve(fileURL).getPath().toString();
+
+			if (!file.exists()) {
+				logger.debug("Configuration file not found. Create it: {}", absolutePath);
+				System.err.println("Configuration file '" + absolutePath
+						+ "' not found. Create it with default value (" + executablePath + ")");
+				file.createNewFile();
+				BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+				bw.write(executablePath);
+				bw.close();
+			} else {
+				logger.debug("Configuration file found: {}", absolutePath);
+				BufferedReader br = new BufferedReader(new FileReader(file));
+				String line = "";
+				while ((line = br.readLine()) != null) {
+					if (line.equals(""))
+						continue;
+					if (line.startsWith("#"))
+						continue;
+					break;
+				}
+				br.close();
+				executablePath = line;
+			}
+			return executablePath;
+		} catch (IOException e) {
+			logger.error(e.getMessage());
+			e.printStackTrace();
+		}
+		return null;
+
+	}
+
+}
