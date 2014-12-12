541ea4b0df30d7fce4a4cceb7c043c9ce67b8fea
Generoso Pagano <generoso.pagano@inria.fr>
2014-12-12 19:49:53 +0100

    script ongoing

--- src/fr.inria.soctrace.maven.master/change_version.sh
+++ src/fr.inria.soctrace.maven.master/change_version.sh
@@ -1,4 +1,5 @@
-#!/bin/bash -x
+File Edit Options Buffers Tools Sh-Script Help                                                                                                                                                                                               
+#!/bin/bash -x                                                                                                                                                                                                                               
 
 if [ $# -lt 1 ]; then
     echo "usage: change_version.sh <new version>"
@@ -8,11 +9,21 @@ fi
 
 NEW=$1
 
+FEATURE="../fr.inria.soctrace.features.framesoc/feature.xml"
+CATEGORY=""
+
 BV="Bundle-Version:.*.qualifier"
 NBV="Bundle-Version: ${NEW}.qualifier"
 
 echo "Updating MANIFEST.MF in all plugins"
 find .. -wholename "*META-INF/MANIFEST.MF" | grep -v "linuxtools" | xargs sed -i s/"$BV"/"$NBV"/
 
+echo "Updating framesoc feature.xml"
+sed s/"version=\".*.qualifier\""/"version=\"${NEW}.qualifier\""/ <$FEATURE > tmp
+mv $FEATURE $FEATURE.bkp
+mv tmp $FEATURE
+rm $FEATURE.bkp
+
+exit
 echo "Update pom.xml in all modules"
-mvn versions:set -DnewVersion="${NEW}-SNAPSHOT"
+mvn versions:set -DnewVersion="${NEW}-SNAPSHOT" -DgenerateBackupPoms=false
