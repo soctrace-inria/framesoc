2e341622f421d701ef94406c67f998d3fa421d5a
Generoso Pagano <generoso.pagano@inria.fr>
2014-12-13 01:23:08 +0100

    finished script

--- src/fr.inria.soctrace.maven.master/change_version.sh
+++ src/fr.inria.soctrace.maven.master/change_version.sh
@@ -1,18 +1,31 @@
-#!/bin/bash                                                                                                                                                                                                                               
+#!/bin/bash                                                                                                                                                         
+# Usage help function
+function usage() {
+    echo "usage: ./change_version.sh <new version>"
+    echo "<new version>: new version in the format x.y.z (where x,y,z are positive integers)"
+}                                     
+                     
+# Parameter check            
 if [ $# -lt 1 ]; then
-    echo "usage: change_version.sh <new version>"
-    echo "<new version>: new version in the format x.y.z"
+    usage
     exit
 fi
 
 NEW=$1
 
+if [[ ! $NEW =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Wrong version format: $NEW"
+    usage
+    exit
+fi
+
+# Constants
 FEATURE="../fr.inria.soctrace.features.framesoc/feature.xml"
 CATEGORY="../fr.inria.soctrace.maven.repository/category.xml"
-
 BV="Bundle-Version:.*.qualifier"
 NBV="Bundle-Version: ${NEW}.qualifier"
 
+# Body
 echo "Updating MANIFEST.MF in all plugins"
 find .. -wholename "*META-INF/MANIFEST.MF" | grep -v "linuxtools" | xargs sed -i s/"$BV"/"$NBV"/
 
